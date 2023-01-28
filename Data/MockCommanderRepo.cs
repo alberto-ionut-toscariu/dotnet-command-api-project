23c47b73cfc87d6c90dc7036261c4e96c7d14ca8
Alberto-Ionut Toscariu <62894046+ElusiveMind22@users.noreply.github.com>
2023-01-28 20:39:18 +0200

    added controller for mock

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,23 @@
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+            new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle & Pan" },
+            new Command { Id = 1, HowTo = "Boil an egg2", Line = "Boil water2", Platform = "Kettle & Pan2" },
+            new Command { Id = 2, HowTo = "Boil an egg3", Line = "Boil water3", Platform = "Kettle & Pan3" }
+            };
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle & Pan" };
+        }
+    }
+}
